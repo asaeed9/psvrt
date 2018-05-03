37d67c97e8a924c2e97c5304f04a383802d42875
ahsan.saeed@gmail.com <ahsan.saeed@gmail.com>
2018-05-03 01:16:46 -0400

    removing original image generation script

--- base_line_exp/base_line.py
+++ base_line_exp/base_line.py
@@ -4,10 +4,30 @@ from __future__ import print_function
 
 import numpy as np
 import tensorflow as tf
+import os
+import cv2
 
-def main(args):
-    tf.logging.set_verbosity(tf.logging.INFO)
-
-
+list_of_imgs = []
+img_dir_same = "./gen_script/data_sample/1"
+img_dir_diff = "./gen_script/data_sample/0"
+def load_data(img_dir):
+	for img in os.listdir(img_dir):
+		print(img)
+    		img = os.path.join(img_dir, img)
+    		print(img)	
+		if not img.endswith(".png"):
+        		continue
+    		a = cv2.imread(img)
+    		
+		if a is None:
+        		print ("Unable to read image{}".format(img))
+        		continue
+    		
+		list_of_imgs.append(a.flatten())
+	train_data = np.array(list_of_imgs)
+	return train_data
+	
 if __name__ == "__main__":
-  tf.app.run()
+	train_data = load_data(img_dir_same)
+	print(train_data.shape()))
+
